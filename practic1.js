70d85fb5b0d8b64adadf14e38e8bb9463a7c1441
sohel-rana411 <pysohelrana411@gmail.com>
2026-07-05 14:51:24 +0600

    first commit

---
+++ practic1.js
@@ -0,0 +1,28 @@
+//***template literals**
+// const age = 21;
+// console.log(`I am ${age} years old!`)
+
+// ***Distructuring
+
+// const student = {
+//     "name":"Sohel Rana",
+//     "age":21,
+//     "dep":"CST",
+//     "semester":"Seven"
+// }
+// const {name} = student;
+// console.log(name);
+// const {age} = student;
+// console.log(`I am ${age} years old!`)
+
+
+const fruits = ["Apple","Banana","Mango"];
+
+const [ff,sf,tf] = fruits;
+console.log(ff,sf,tf)
+
+
+
+
+// ***rest operator
+
---
+++ script.js
@@ -0,0 +1,4 @@
+function myFunction(){
+    const btn = document.querySelector("#myId");
+    btn.innerHTML = "My Name I s Sohel Rana.I am a Web Developer.I am a Student,in Diploma Engineering"
+}
